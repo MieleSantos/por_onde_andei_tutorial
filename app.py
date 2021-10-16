6b31b59657973656dfb2b0c4af3741be883e2d04
mielesantos <mielesnts@gmail.com>
2021-10-16 12:27:09 -0300

    criado novo endpoint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from utils.maps import create_map
 
 app = Flask(__name__)
 
@@ -12,3 +13,12 @@ def home():
 @app.get('/oops')
 def oops():
     return render_template('oops.html')
+
+
+@app.get('/ping')
+def update_map():
+    created = create_map()
+    if created:
+        return render_template("index.html")
+
+    return redirect(url_for("oops"))
