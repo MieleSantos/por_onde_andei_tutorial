ac0eabf7e950d6ad3c0117146cfa55241d73647b
mielesantos <mielesnts@gmail.com>
2021-10-16 12:01:40 -0300

    criando funções para trabalha com mapa

--- utils/maps.py
+++ utils/maps.py
@@ -1,3 +1,4 @@
+import folium
 import pandas as pd
 
 
@@ -7,3 +8,21 @@ def load_data():
 
 def save_data(df):
     df.to_csv("places.csv")
+
+
+def create_standard_map():
+    center = (13.133932434766733, 16.103938729508073)
+    return folium.Map(center, zoom_start=3)
+
+
+def add_maker(my_map, df):
+    for _, place in df.iterrows():
+        folium.Marker(
+            location=[
+                place['latitude'],
+                place['longitude']
+                ],
+            tooltip=place['name'],
+            popup=place['name']
+        ).add_to(my_map)
+    return my_map
